3352b504418a334f0f7eb2dc70c04e53c3103908
wanmin <min.wan@yoho.cn>
2016-09-12 11:12:24 +0800

    db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gezlife_vr',
+    'dsn' => 'mysql:host=192.168.16.253;dbname=gezlife_vr',
     'username' => 'vr',
     'password' => '123456',
     'charset' => 'utf8',
